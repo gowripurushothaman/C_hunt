93efe7f6997a54955e84dbb4e7af04484fe0e5f2
gowripurushothaman <gowrikalpana27@gmail.com>
2017-07-06 23:15:25 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+char a,i;
+printf("enter the character");
+scanf("%c",&c);
+for(i='a';i<'z';i++)
+{
+printf("ascii value of %c is %d",i,i);
+}
+return 0;
+}
